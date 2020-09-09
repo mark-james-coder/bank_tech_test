dc9e7f2331f798c3a25c6aabc9fe666bce1d7fa0
Mark <markjpoole@yahoo.com>
2020-09-09 23:05:16 +0100

    Create account class that initializes with a balance of zero

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,6 @@ require 'account'
 describe Account do
   it 'initalises with a balance of zero' do
     account = Account.new
-    expect(account.balance).to eq '0'
+    expect(account.balance).to eq 0
   end
 end
